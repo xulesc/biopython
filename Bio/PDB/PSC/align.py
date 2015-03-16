1b9eaddc6162fb5490203dd2e6297218d53d8477
Anuj Sharma <anuj.sharma80@gmail.com>
2015-03-16 14:02:27 +0200

    corrected dp implementation.

--- Bio/PDB/PSC/align.py
+++ Bio/PDB/PSC/align.py
@@ -33,9 +33,35 @@ class Munkres:
     print 'a = %s' %a
     print 'c = %s' %(self.run_munkres(a))
 
+class DP:
+    #http://www.avatar.se/molbioinfo2001/dynprog/adv_dynamic.html
+    def dp(self, gapcost, S):
+        w = gapcost; (rows, cols) = S.shape
+        # init
+        M = np.zeros((rows + 1, cols + 1), dtype=np.float)
+        ## fill
+        for i in xrange(1, rows + 1):
+            for j in xrange(1, cols + 1):
+                M[i][j] = max(M[i-1][j-1]+S[i-1][j-1], M[i][j-1]+w, M[i-1][j]+w)
+        X = rows; Y = cols
+        ## traceback
+        alignment = []
+        while X > 0 and Y > 0:
+            ## diagonal, left, up
+            u = [(X-1,Y-1),(X-1,Y),(X,Y-1)]
+            v = np.array([M[X-1][Y-1] + S[X-1][Y-1], M[X-1][Y] + w, M[X][Y-1] + w])
+            ## which value is equal to the current value
+            i = np.where(v==M[X][Y])[0][0]
+            (X,Y) = u[i]
+            if i == 0:
+                alignment.append((X, Y))
+        alignment.reverse()
+        return alignment                                                                                                                    
+
 class Align:
     def __init__(self):
 	self.munkres = Munkres()
+        self.dp = DP()
       
     def non_sequential(self, coords1, coords2):
 	dist_matrix = distance.cdist(coords1, coords2, 'euclidean').astype(np.int32)
@@ -51,33 +77,6 @@ class Align:
             retval[-1].append(0)
         return retval  
 
-    #http://www.avatar.se/molbioinfo2001/dynprog/adv_dynamic.html
-    def _dp(self, gapcost, S):
-        w = gapcost; (rows, cols) = S.shape
-        # init
-        M = np.zeros((rows + 1, cols + 1), dtype=np.float)
-        ## fill 
-        for i in xrange(1, rows + 1):
-            for j in xrange(1, cols + 1):
-                M[i][j] = max(M[i-1][j-1]+S[i-1][j-1], M[i][j-1]+w, M[i-1][j]+w)
-        (X, Y) = np.where(M == M.max()); X = X[len(X)-1]; Y = Y[len(Y)-1]
-        ## traceback
-        path = []
-        while X > 0 and Y > 0:
-            diag = M[X-1][Y-1]
-            up = M[X-1][Y]
-            left = M[X][Y-1]
-            if diag >= up and diag >= left:
-                path.append((X - 1, Y - 1))
-                X -= 1
-                Y -= 1
-            elif up >= diag and up >= left:
-                X -= 1
-            else:
-                Y -= 1
-        path.reverse()
-        return path    
-	
     def sequential(self, coords1, coords2):
         gap_penalty = -0.6
         dist_matrix = distance.cdist(coords1, coords2, 'euclidean').astype(np.float)
@@ -85,7 +84,9 @@ class Align:
         Lmin = min(coords1.shape[0], coords2.shape[0])
         d0Lmin = (1.24 * ((1 + math.cos(Lmin-1.5)) ** (1 / 3.0))) ** 2
         siml_matrix = 1 / (1 + (dist_matrix * 2)/d0Lmin)
-        return self._dp(gap_penalty, siml_matrix)
+        # inverse of rmsd
+        #siml_matrix = 1 - dist_matrix / dist_matrix.max()
+        return self.dp.dp(gap_penalty, siml_matrix)
       
 if __name__ == '__main__':
     print 'local test'
