c8d608ba53f8fee92d00d9cd4b311e162ea0963c
Anuj Sharma <anuj.sharma80@gmail.com>
2015-03-17 19:41:05 +0200

    dp2 fix

--- Bio/PDB/PSC/align.py
+++ Bio/PDB/PSC/align.py
@@ -58,6 +58,51 @@ class DP:
         alignment.reverse()
         return alignment                                                                                                                    
 
+    def dp2(self, gapcost, S):
+        w = gapcost; (rows, cols) = S.shape
+        ## init
+        dir = np.zeros((rows+1, cols+1), dtype=np.float)
+        val = np.zeros((rows+1, cols+1), dtype=np.float)
+        ## decide matrix and path
+        for i in xrange(1, rows+1):
+            for j in xrange(1, cols+1):
+                D = val[i-1][j-1] + S[i-1][j-1]
+                H = val[i-1][j]
+                if dir[i-1][j] == 1:
+                    H = H + gapcost
+                V = val[i][j-1]
+                if dir[i][j-1] == 1:
+                    V = V + gapcost
+            
+                if D >= H and D >= V:
+                    dir[i][j] = 1
+                    val[i][j] = D
+                else:
+                    dir[i][j] = 0
+                    val[i][j] = max(V, H)
+        #print dir
+        #print val
+        ## extract the alignment
+        (i, j) = (rows, cols); path = []
+        while i > 0 and j > 0:
+            if dir[i][j] == 1:
+                path.append((i - 1, j - 1))
+                i -= 1
+                j -= 1
+            else:
+                H = val[i-1][j]
+                if dir[i-1][j] == 1:
+                    H = H + gapcost
+                V = val[i][j-1]
+                if dir[i][j-1] == 1:
+                    V = V + gapcost
+                if V >= H:
+                    j -= 1
+                else:
+                    i -= 1
+        path.reverse()
+        return path
+
 class Align:
     def __init__(self):
 	self.munkres = Munkres()
@@ -86,7 +131,7 @@ class Align:
         siml_matrix = 1 / (1 + (dist_matrix * 2)/d0Lmin)
         # inverse of rmsd
         #siml_matrix = 1 - dist_matrix / dist_matrix.max()
-        return self.dp.dp(gap_penalty, siml_matrix)
+        return self.dp.dp2(gap_penalty, siml_matrix)
       
 if __name__ == '__main__':
     print 'local test'
